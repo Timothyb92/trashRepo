d2dbd9a1d8e7bccbb7c8e54b4e418acd3a09f297
Timothy Bedsole <timbedsole@gmail.com>
2018-08-04 20:59:20 -0400

    made objects to hold dynamic results from ebay api. getting failures from ajax request and not sure why

--- working flickety carousel/mutt.js
+++ working flickety carousel/mutt.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
     var $carousel = $('.carousel').flickity()
   .flickity('next')
   .flickity( 'select', 4 );
+  var smallDogProducts = {};
+    var mediumDogProducts = {};
+    var largeDogProducts = {};
 
     var petUrl = "http://api.petfinder.com/pet.find";
     var petApiKey = "aaf7ea34460505b8e7841f0512aae7a4"
@@ -30,7 +33,7 @@ $(document).ready(function(){
                 format: "json"
             }
         }).then(response=> {
-            // console.log(response);
+            console.log(response);
             // var dogName = response.petfinder.pets.pet[1].name.$t;
             // var dogAge = response.petfinder.pets.pet[1].age.$t;
             // var dogSex = response.petfinder.pets.pet[1].sex.$t;
@@ -43,13 +46,11 @@ $(document).ready(function(){
 
             //Loops through each dog pulled back from ajax request
             response.petfinder.pets.pet.forEach(function(j){
-                console.log(j.media.photos.photo);
                 //This loop goes through the first 5 images for each dog
                 //Each image pulled from the API has the same image in 5 different sizes
                 //This for loop looks for the first full sized image of the dog
                 for (l = 0; l < 5 ; l++){
                     if (j.media.photos.photo[l]["@size"] == "x"){
-                        console.log(j.media.photos.photo[l].$t);
                         //Sets the first full size image of the dog to uniqueDogImg
                         var uniqueDogImg = j.media.photos.photo[l].$t;
                         //Creatin a div to hold the iamge
@@ -66,32 +67,32 @@ $(document).ready(function(){
                 }
 
             })
-            photoGallery.forEach(function(i){
-                // THIS LOOP IS WORKING ON photoGallery VARIABLE
-                //VARIABLE IS ONLY SET TO 1 DOG IN THE ARRAY OF 25 DOGS
-                //THIS LOOP IS MEANT TO PUSH EVERY ADDITIONAL IMAGE OF ONE SPECIFIC DOG
-                //TO THE INNER CAROUSEL INSIDE THE MODAL
-                if (i["@size"] == "x"){
-                    console.log(i.$t)
-                    var newCarouselDiv = $("<div>");
-                    var newCarImg = $("<img>");
-                    newCarouselDiv.addClass("carousel-cell");
-                    newCarImg.attr("src", i.$t);
-                    newCarouselDiv.html(newCarImg);
-                    // $carousel.flickity( 'append', newCarouselDiv )
-                }
-            })
+            // photoGallery.forEach(function(i){
+            //     // THIS LOOP IS WORKING ON photoGallery VARIABLE
+            //     //VARIABLE IS ONLY SET TO 1 DOG IN THE ARRAY OF 25 DOGS
+            //     //THIS LOOP IS MEANT TO PUSH EVERY ADDITIONAL IMAGE OF ONE SPECIFIC DOG
+            //     //TO THE INNER CAROUSEL INSIDE THE MODAL
+            //     if (i["@size"] == "x"){
+            //         console.log(i.$t)
+            //         var newCarouselDiv = $("<div>");
+            //         var newCarImg = $("<img>");
+            //         newCarouselDiv.addClass("carousel-cell");
+            //         newCarImg.attr("src", i.$t);
+            //         newCarouselDiv.html(newCarImg);
+            //         // $carousel.flickity( 'append', newCarouselDiv )
+            //     }
+            // })
 
-            $("#dogModalPic").addClass("img-responsive");
-            $("#modalTitle").text(dogName);
-            $("#description").html(dogDescription);
-            $("#details").append($("<p>").addClass("col-md-4").text("Age: " + dogAge));
-            $("#details").append($("<p>").addClass("col-md-4").text("Size: " + dogSize));
-            $("#details").append($("<p>").addClass("col-md-4").text("Sex: " + dogSex));
+            // $("#dogModalPic").addClass("img-responsive");
+            // $("#modalTitle").text(dogName);
+            // $("#description").html(dogDescription);
+            // $("#details").append($("<p>").addClass("col-md-4").text("Age: " + dogAge));
+            // $("#details").append($("<p>").addClass("col-md-4").text("Size: " + dogSize));
+            // $("#details").append($("<p>").addClass("col-md-4").text("Sex: " + dogSex));
         });
     });
     
-    var searchstring = "medium dog toys";
+    var searchString = "";
     var ebayURL = "http://svcs.ebay.com/services/search/FindingService/v1";
       ebayURL += "?OPERATION-NAME=findCompletedItems";
       ebayURL += "&SERVICE-VERSION=1.13.0";
@@ -102,33 +103,64 @@ $(document).ready(function(){
       ebayURL += "&REST-PAYLOAD";
       ebayURL += "&paginationInput.pageNumber=1";
       ebayURL += "&paginationInput.entriesPerPage=10";
-      ebayURL += "&keywords=" + searchstring;
+      ebayURL += "&keywords=" + searchString;
       ebayURL += "&sortOrder=StartTimeNewest";
     
+
+      searchString = "medium dog toys";
       $.ajax({
         type: "GET",
         url: ebayURL,
         dataType: "jsonp",
-        // success: function(res){
-        //   console.log(res);
-          
-        // }
       }).then(response => {
-        //   console.log(response)
-          for (i = 0; i < 3; i++){
-            ebayItemImg = response.findCompletedItemsResponse[0].searchResult[0].item[i].galleryURL[0];
-            ebayItemLink = response.findCompletedItemsResponse[0].searchResult[0].item[i].viewItemURL[0];
-            var productLink = $("<a>");
-            var productImg = $("<img>");
-            productLink.attr("href", ebayItemLink)
-            productImg.attr("src", ebayItemImg);
-            productLink.html(productImg);
-    
-            $("#products").append(productLink)
-        }
+          console.log(response);
+        //   ebayItemImg = response.findCompletedItemsResponse[0].searchResult[0].item[0].galleryURL[0];
+        //   ebayItemLink = response.findCompletedItemsResponse[0].searchResult[0].item[0].viewItemURL[0];
+                //   var productLink = $("<a>");
+                //   var productImg = $("<img>");
+                //   productLink.attr("href", ebayItemLink)
+                //   productImg.attr("src", ebayItemImg);
+                //   productLink.html(productImg);
+        // mediumDogProducts.itemOneImg = ebayItemImg;
+        // mediumDogProducts.itemOneUrl = ebayItemLink;
       });ebayURL += "&sortOrder=StartTimeNewest";
-        // var myElement = document.getElementById(‘myCarousel’);
-        // var mc = new Hammer(myElement);
+
+      searchString = "medium dog leash";
+      $.ajax({
+        type: "GET",
+        url: ebayURL,
+        dataType: "jsonp",
+      }).then(response => {
+        console.log(response);
+            // ebayItemImg = response.findCompletedItemsResponse[0].searchResult[0].item[0].galleryURL[0];
+            // ebayItemLink = response.findCompletedItemsResponse[0].searchResult[0].item[0].viewItemURL[0];
+                    // var productLink = $("<a>");
+                    // var productImg = $("<img>");
+                    // productLink.attr("href", ebayItemLink)
+                    // productImg.attr("src", ebayItemImg);
+                    // productLink.html(productImg);
+            // mediumDogProducts.itemTwoImg = ebayItemImg;
+            // mediumDogProducts.itemTwoUrl = ebayItemLink;
+      });ebayURL += "&sortOrder=StartTimeNewest";
+
+      searchString = "medium dog food";
+      $.ajax({
+        type: "GET",
+        url: ebayURL,
+        dataType: "jsonp",
+      }).then(response => {
+        console.log(response);
+            // ebayItemImg = response.findCompletedItemsResponse[0].searchResult[0].item[0].galleryURL[0];
+            // ebayItemLink = response.findCompletedItemsResponse[0].searchResult[0].item[0].viewItemURL[0];
+                    // var productLink = $("<a>");
+                    // var productImg = $("<img>");
+                    // productLink.attr("href", ebayItemLink)
+                    // productImg.attr("src", ebayItemImg);
+                    // productLink.html(productImg);
+            // mediumDogProducts.itemThreeImg = ebayItemImg;
+            // mediumDogProducts.itemThreeUrl = ebayItemLink;
+      });ebayURL += "&sortOrder=StartTimeNewest";
+      console.log(mediumDogProducts);
         
         $("#myCarousel").on( "swipeleft", function( event )
         {
